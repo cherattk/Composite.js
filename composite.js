3b02bef89acef5f358fcb91fdd8a0be192d68df6
cheratt karim <karim.cheratt@yahoo.ca>
2017-12-04 22:07:49 -0500

    add form and notification

--- composite.js
+++ composite.js
@@ -8,9 +8,10 @@
 
 class _Composite {
     
-    constructor() {      
+    constructor() {
         this.component = {};
         this.observable = {};
+        this.shared_data = {};
     }   
     
     buildURL(view_path){
@@ -27,25 +28,9 @@ class _Composite {
       return this.component[name];
     }
   
-    configComponent(c){
-        let self = this;        
-        $.when( 
-            self.settingView(c.name , c.view), 
-            self.settingData(c.name , c.data))
-        .then(function(){
-                // Composite.updateComponentData()
-                //MUST be called after registering observer
-                //@see Composite.registerObserver();                 
-                c.eventHandler(this);
-                self.updateRender(c.name);
-            });
-        
-        return c;
-    }
-    
     settingData(component_name , data){
         
-        if(typeof data === 'string'){            
+        if(data && typeof data === 'string'){            
             let self = this;
             return $.getJSON(this.buildURL(data) , function(response_data){
                 self.component[component_name]['data'] = response_data;
@@ -53,77 +38,104 @@ class _Composite {
         }
     }
     
-    settingView(component_name , view){
+    settingView(component_name , template){
         
-        let self = this;
-        if(typeof view === 'function'){
-            self.component[component_name]['view'] = view();
+        var self = this;
+        if(typeof template === 'function'){
+            self.component[component_name]['view']['template'] = template();
             
-        }else if (typeof view === 'string'){
-            let url = this.buildURL(view);
+        }else if (typeof template === 'string'){
+            let url = this.buildURL(template);
             return $.get( url , function(html){
-                self.component[component_name]['view'] = html;
+                self.component[component_name]['view']['template'] = html;
             });
         }
     }
     
-    registerObserver(c){
+    settingComponent(c){
         
-        if(Array.isArray(c.observable)){
-            c.observable.forEach(function(subject){
-                if(typeof this.observable[subject] === "undefined"){
-                    this.observable[subject] = [c.name];
+        var self = this;        
+        this.component[c.name] = c;
+        
+        $.when(
+            self.settingView(c.name , c.view.template), 
+            self.settingData(c.name , c.data))
+        .then(function(){                
+                self.renderComponent(c.name);
+                self.component[c.name].init(self);
+        });
+    }
+    
+    updateData(component_name , data){
+        this.shared_data[component_name] = data;
+        this.notifyObserver(component_name);
+    }
+    
+    registerObserver(component){
+        
+        if(Array.isArray(component.listen)){
+            
+            var listen = component.listen;            
+            for (var i = 0, max = listen.length - 2 ; i <= max; i++) {
+                if(typeof this.observable[listen[i]] === "undefined"){
+                    this.observable[listen[i]] = [component];
                 }
                 else{
-                    this.observable[subject].push(c.name);
-                }
-            },this);
+                    this.observable[listen[i]].push(component);
+                }   
+            }
         }
     }
     
-    addComponent(_c){        
+    addComponent(_c){
         
-        var c = Object.assign({
-            name : '',
-            view : '',
-            parent : '',
-            data : '',
-            eventHandler : function(){},
-            observable : []
-        } , _c);
+        var component = $.extend(true,
+            {
+                name : '', //required
+                view : {
+                    anchor : '',  // container's id into which the view is inserted
+                    template: ''
+                },
+                data : '',
+                init : function(){},
+                listen : ''
+            } , _c );
         
-        /**
-         * registerObserver() MUST be called before configComponent()
-        */        
-        this.registerObserver(c);
-        this.component[c.name] = c;
-        this.configComponent(c);
-       
+        if(typeof _c.listen !== " undefined"){
+            component.listen = _c.listen;
+        }
+        this.settingComponent(component);
+        this.registerObserver(component);
     }
     
-    notify(component_name){
+    notifyObserver(component_name){
+        
         if(typeof this.observable[component_name] !== "undefined"){
-            this.observable[component_name].forEach(function(observer_name) {
-                this.render(observer_name);
+            
+            this.observable[component_name].forEach(function(observer) {
+                
+                let lastElement = observer.listen[observer.listen.length-1];
+                lastElement.call(
+                        observer,
+                        this, // Composite Object
+                        { name :  component_name,
+                          data : this.shared_data[component_name]
+                        });
+                    
+        
             },this);            
         }
     }
   
-    updateRender(component_name){ 
-        this.render(component_name);
-        this.notify(component_name);
-    }
-  
-    render(component_name){
+    renderComponent(component_name){
         
-        //************************ Mustache Rendering *****************
         var c = this.component[component_name];        
-        var parent = document.getElementById(c.parent);        
-        if(!parent){return;}
+        var anchor = document.getElementById(c.view.anchor);        
+        if(!anchor){return;}
         
-        parent.innerHTML = Mustache.render(c.view, c.data); 
-        
-        //**************************************************************/
+        //******* Mustache Rendering *****************
+        anchor.innerHTML = Mustache.render(c.view.template, c.data);       
+        //*********************************************/
     }
 }
 
--- demo/component/module.js
+++ demo/component/module.js
@@ -1,41 +1,69 @@
 MyModule = [
     {  
         name : 'head',
-        view : "demo/component/head.html",
-        parent : 'component-1',
+        view : {
+            anchor : 'anchor-head',
+            template: "demo/component/head.html"
+        },
         data : {
-                title : "Todo List",
+                title : "Composite Todo List",
                 message : "One Task at a Time"
-            },
-        eventHandler : function(Composite) {
-
-            //console.log(this.name + ' update his data');    
+            }
+    },    
+    {
+        name : 'task-form',
+        view : {
+            anchor : 'anchor-form',
+            template : function(){
+                return `<form id="task-form">
+                        <input type="text" name="label"/>
+                        <input type="submit" value="save"/>
+                      </form>`;
+            }
+        },
+        
+        init : function(composite){            
+            form = document.getElementById('task-form');
+            var self = this;
+            form.onsubmit = function(e){
+                e.preventDefault();
+                var data = {
+                    id : '1111',
+                    status : 'todo',
+                    label : this.elements['label'].value
+                };
+                composite.updateData(self.name , data);
+            };
         }
-
     },
     {  
         name : 'list',
-        view : function(){
-            return `<ul id="todo-list" class="todo-list">
-                    {{ #todo }}
-                      <li class="{{ status }}" data-taskid="{{ id }}">
-                        {{ label }}
-                      </li>
-                    {{ /todo }}
-                </ul>`;
+        view : {            
+            anchor : 'anchor-list',
+            template : function(){
+                return `<ul id="todo-list" class="todo-list">
+                        {{ #todo }}
+                          <li class="{{ status }}" data-taskid="{{ id }}">
+                            {{ label }}
+                          </li>
+                        {{ /todo }}
+                    </ul>`;
+            }
         },
         data : "demo/component/data.json",
-        parent : 'component-2',
-        eventHandler : function(){
-            var list = document.getElementById(this.parent);
+        init : function(){
+            var list = document.getElementById(this.view.anchor);
                     list.addEventListener('click', function(ev) {
                     if (ev.target.tagName === 'LI') {
                       ev.target.classList.toggle('done');
                     }
                 }, false);
         },
-        observable : ['head']
-
+        listen : ['task-form' , function(composite , notification){
+                
+            this.data.todo.push(notification.data);
+            composite.renderComponent(this.name);
+        }]
     }
 ];
 
